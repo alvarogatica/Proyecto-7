30036b373ecbc618106837780b7e9ea47d0c8603
alvarogatica <alvaroandresgaticariquelme@gmail.com>
2025-07-04 02:50:23 -0400

    styles

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,23 @@
-import PurseList from "../Purse/List"
-
+import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
     <>
-      Home
-      <PurseList />
+      <main className="text-center px-4 mt-24 mx-auto">
+        <h1 className="text-5xl font-extrabold text-gray-900">
+          urdupes.cl
+        </h1>
+        <P className="mt-3 mx-auto text-gray-500">
+          Tu tienda de accesorios femeninos
+        </P>
+        <section className="mt-16 mx-auto max-w-md">
+          <article>
+            <Link to="/purses" className="btn-product">
+              
+            </Link>
+          </article>
+        </section>
+      </main>
     </>
   )
 }
