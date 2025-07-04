f4668b84ffecad4aaa1f7c4552546a24a2400a5d
alvarogatica <alvaroandresgaticariquelme@gmail.com>
2025-07-03 20:23:17 -0400

    agregando tailwind

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,10 +1,10 @@
-
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <footer className="text-gray-600 text-xs max-w-5xl mx-auto text-center px-4 pt-24 pb-8">
+      <p>&copy; 2025</p>
+      <p>Tienda electronica de accesorios femeninos</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Purse/List/index.jsx
+++ src/components/Purse/List/index.jsx
@@ -12,7 +12,7 @@ const PurseList = () => {
     <div>
       PurseList
       {purses.map((purse) => (
-        <div key={purse.id}>
+        <div key={purse._id}>
           <h1>{purse.name}</h1>
           <p>{purse.price}</p>
         </div>
